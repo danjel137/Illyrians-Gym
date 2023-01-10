9c7688e9a780f425cb7cf076119f4b7ae4b3d0ac
Isuf <isuf.muca@crystal-system.eu>
2023-01-10 14:01:12 +0100

    added a  completed coder for user class

--- src/main/java/model/utilities/UserCoder.java
+++ src/main/java/model/utilities/UserCoder.java
@@ -8,7 +8,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 public class UserCoder extends StructuredCoder<User> {
 
@@ -41,132 +40,165 @@ public class UserCoder extends StructuredCoder<User> {
 //        STRING_UTF_8_CODER.encode(value.getEndTimeSubscription(), outStream);
 
 
-        BOOLEAN_CODER.encode(Objects.nonNull(value), outStream);
+        BOOLEAN_CODER.encode(value != null, outStream);
 
-        if (value != null) {
+        BIG_ENDIAN_INT_CODER.encode(value.getUserId(), outStream);
 
-            BIG_ENDIAN_INT_CODER.encode(value.getUserId(), outStream);
+        BIG_ENDIAN_INT_CODER.encode(value.getGymId(), outStream);
 
-            BIG_ENDIAN_INT_CODER.encode(value.getGymId(), outStream);
-
-            if (value.getFirstName() != null) {
-                STRING_UTF_8_CODER.encode(value.getFirstName(), outStream);
-            }
-
-            if (value.getLastName() != null) {
-                STRING_UTF_8_CODER.encode(value.getLastName(), outStream);
-            }
-
-            if (value.getEmail() != null) {
-                STRING_UTF_8_CODER.encode(value.getEmail(), outStream);
-            }
-
-            if (value.getPassword() != null) {
-                STRING_UTF_8_CODER.encode(value.getPassword(), outStream);
-            }
-
-            if (value.getUserType() != null) {
-                STRING_UTF_8_CODER.encode(value.getUserType(), outStream);
-            }
-
-            if (value.getBirthday() != null) {
-                STRING_UTF_8_CODER.encode(value.getBirthday(), outStream);
-            }
-
-            if (value.getGender() != null) {
-                STRING_UTF_8_CODER.encode(value.getGender(), outStream);
-            }
-
-            if (value.getPhoneNumber() != null) {
-                STRING_UTF_8_CODER.encode(value.getPhoneNumber(), outStream);
-            }
-
-            if (value.getTimeDateRegistered() != null) {
-                STRING_UTF_8_CODER.encode(value.getTimeDateRegistered(), outStream);
-            }
-
-            if (value.getTrainerInstagramAccount() != null) {
-                STRING_UTF_8_CODER.encode(value.getTrainerInstagramAccount(), outStream);
-            }
-
-            if (value.getTrainerTitle() != null) {
-                STRING_UTF_8_CODER.encode(value.getTrainerTitle(), outStream);
-            }
-
-            if (value.getTrainerDescription() != null) {
-                STRING_UTF_8_CODER.encode(value.getTrainerDescription(), outStream);
-            }
-
-            if (value.getEndTimeSubscription() != null) {
-                STRING_UTF_8_CODER.encode(value.getEndTimeSubscription(), outStream);
-            }
+        if (value.getFirstName() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getFirstName(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
         }
+
+        if (value.getLastName() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getLastName(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
+        }
+
+        if (value.getEmail() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getEmail(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
+        }
+
+        if (value.getPassword() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getPassword(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
+        }
+
+        if (value.getUserType() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getUserType(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
+        }
+
+        if (value.getBirthday() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getBirthday(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
+        }
+
+        if (value.getGender() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getGender(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
+        }
+
+        if (value.getPhoneNumber() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getPhoneNumber(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
+        }
+
+        if (value.getTimeDateRegistered() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getTimeDateRegistered(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
+        }
+
+        if (value.getTrainerInstagramAccount() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getTrainerInstagramAccount(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
+        }
+
+        if (value.getTrainerTitle() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getTrainerTitle(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
+        }
+
+        if (value.getTrainerDescription() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getTrainerDescription(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
+        }
+
+        if (value.getEndTimeSubscription() != null) {
+            BOOLEAN_CODER.encode(true, outStream);
+            STRING_UTF_8_CODER.encode(value.getEndTimeSubscription(), outStream);
+        } else {
+            BOOLEAN_CODER.encode(false, outStream);
+        }
+
 
     }
 
     @Override
     public User decode(InputStream inStream) throws IOException {
+
         if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
 
             User user = new User();
 
-            if (BIG_ENDIAN_INT_CODER.decode(inStream) != null) {
-                user.setUserId(BIG_ENDIAN_INT_CODER.decode(inStream));
-            }
+            user.setUserId(BIG_ENDIAN_INT_CODER.decode(inStream));
+            user.setGymId(BIG_ENDIAN_INT_CODER.decode(inStream));
 
-            if (BIG_ENDIAN_INT_CODER.decode(inStream) != null) {
-                user.setGymId(BIG_ENDIAN_INT_CODER.decode(inStream));
-            }
-
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setFirstName(STRING_UTF_8_CODER.decode(inStream));
             }
 
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setLastName(STRING_UTF_8_CODER.decode(inStream));
             }
 
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setEmail(STRING_UTF_8_CODER.decode(inStream));
             }
 
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setPassword(STRING_UTF_8_CODER.decode(inStream));
             }
 
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setUserType(STRING_UTF_8_CODER.decode(inStream));
             }
 
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setBirthday(STRING_UTF_8_CODER.decode(inStream));
             }
 
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setGender(STRING_UTF_8_CODER.decode(inStream));
             }
 
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setPhoneNumber(STRING_UTF_8_CODER.decode(inStream));
             }
 
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setTimeDateRegistered(STRING_UTF_8_CODER.decode(inStream));
             }
 
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setTrainerInstagramAccount(STRING_UTF_8_CODER.decode(inStream));
             }
 
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setTrainerTitle(STRING_UTF_8_CODER.decode(inStream));
             }
 
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setTrainerDescription(STRING_UTF_8_CODER.decode(inStream));
             }
 
-            if (STRING_UTF_8_CODER.decode(inStream) != null) {
+            if (Boolean.TRUE.equals(BOOLEAN_CODER.decode(inStream))) {
                 user.setEndTimeSubscription(STRING_UTF_8_CODER.decode(inStream));
             }
             return user;
@@ -186,4 +218,39 @@ public class UserCoder extends StructuredCoder<User> {
         BOOLEAN_CODER.verifyDeterministic();
     }
 }
+
+
+//
+//public class JsonCoder<T> extends AtomicCoder<T> {
+//    private static final long serialVersionUID = -1L;
+//
+//    private static final Coder<Boolean> BOOLEAN_CODER = BooleanCoder.of();
+//    private static final Coder<String> STRING_CODER = StringUtf8Coder.of();
+//
+//    private Class<T> clazz;
+//
+//    @Override
+//    public void encode(T value, OutputStream outStream) throws IOException {
+//        if (value == null) {
+//            BOOLEAN_CODER.encode(Boolean.FALSE, outStream);
+//        } else {
+//            BOOLEAN_CODER.encode(Boolean.TRUE, outStream);
+//            STRING_CODER.encode(JsonUtil.toJsonString(value), outStream);
+//        }
+//    }
+//
+//    @Override
+//    @Nullable
+//    public T decode(InputStream inStream) throws IOException {
+//        if (BOOLEAN_CODER.decode(inStream)) {
+//            return JsonUtil.serializeToObject(STRING_CODER.decode(inStream), clazz);
+//        } else {
+//            return null;
+//        }
+//    }
+//
+//    public static <T> JsonCoder<T> of(Class<T> clazz) {
+//        return new JsonCoder<>(clazz);
+//    }
+//}
 
